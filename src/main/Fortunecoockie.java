9931c7fc0c9a0e9205c3d2da9ac472e13c24ead0
TetianaHolovchenko <134524399+TetianaHolovchenko@users.noreply.github.com>
2023-05-25 19:58:09 +0000

    Pending changes exported from your codespace

---
+++ src/main/Fortunecoockie.java
@@ -0,0 +1,13 @@
+public class Fortunecoockie {
+    public static void main(String[] args) {
+        //Declare the variable
+        String coockie;
+
+        //Assign a value to the variable
+        coockie = "Success expected";
+
+        //Display the message to user
+        System.out.println(coockie);
+    
+    }
+}
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,8 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        //Declare a variable
+        String drink = "coffee";
+        System.out.println("I want "  +drink);
     }
 }
